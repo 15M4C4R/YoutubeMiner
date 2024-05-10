e2b5827d33fe544acdb234d50bd7823f7211dfb4
alvgarlop <alvgarlop@alum.us.es>
2024-05-10 18:31:51 +0200

    POST YoutubeController

--- src/main/java/AISS/YoutubeMiner/controllers/YoutubeController.java
+++ src/main/java/AISS/YoutubeMiner/controllers/YoutubeController.java
@@ -4,7 +4,11 @@ import AISS.YoutubeMiner.modelsVideo.ChannelVideo.ChannelVideo;
 import AISS.YoutubeMiner.service.ChannelService;
 import AISS.YoutubeMiner.transformation.ChannelTransformation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/youtubeminer")
@@ -13,8 +17,11 @@ public class YoutubeController {
     @Autowired
     ChannelService service;
 
+    @Autowired
+    RestTemplate restTemplate;
 
-    final String videoMinerUri = "http://localhost:8080/api/videoMiner/videos";
+
+        final String videoMinerUri = "http://localhost:8080/api/videoMiner/videos";
 
     @GetMapping("/{id}")
     public ChannelVideo getChannel(@PathVariable String id,
@@ -23,6 +30,13 @@ public class YoutubeController {
         return ChannelTransformation.transformChannel(service.channelSearch(id, maxVideos, maxComments));
     }
 
-
+    @PostMapping("/{id}")
+    public ChannelVideo sendChannel(@PathVariable String id,  @RequestParam(required = false,defaultValue = "10") int maxVideos,
+                                    @RequestParam(required = false,defaultValue = "10") int maxComments){
+        ChannelVideo canal = ChannelTransformation.transformChannel(service.channelSearch(id, maxVideos, maxComments));
+        HttpEntity<ChannelVideo> request = new HttpEntity<>(canal);
+        ResponseEntity<ChannelVideo> response = restTemplate.exchange(videoMinerUri, HttpMethod.POST,request, ChannelVideo.class);
+        return response.getBody();
+    }
 
 }
