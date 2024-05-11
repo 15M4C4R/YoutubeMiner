e2bef99af14e18e4a193f5bc3c3bfd51d35e9e8b
alvgarlop <alvgarlop@alum.us.es>
2024-05-11 20:37:10 +0200

    YoutubeMiner documentation

--- src/main/java/AISS/YoutubeMiner/controllers/YoutubeController.java
+++ src/main/java/AISS/YoutubeMiner/controllers/YoutubeController.java
@@ -3,14 +3,18 @@ package AISS.YoutubeMiner.controllers;
 import AISS.YoutubeMiner.modelsVideo.ChannelVideo.ChannelVideo;
 import AISS.YoutubeMiner.service.ChannelService;
 import AISS.YoutubeMiner.transformation.ChannelTransformation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+@Tag(name = "Youtube", description = "Youtube management")
 @RestController
 @RequestMapping("/youtubeminer")
 public class YoutubeController {
@@ -24,6 +28,16 @@ public class YoutubeController {
 
         final String videoMinerUri = "http://localhost:8080/videominer/channels";
 
+
+    @Operation(
+            description = "Get a channel by specifying its id",
+            tags = {"channel", "get"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = ChannelVideo.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema())})
+    })
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{id}")
     public ChannelVideo getChannel(@PathVariable String id,
                                    @RequestParam(required = false,defaultValue = "10") int maxVideos,
@@ -31,6 +45,16 @@ public class YoutubeController {
         return ChannelTransformation.transformChannel(service.channelSearch(id, maxVideos, maxComments));
     }
 
+
+    @Operation(
+            description = "Create a channel",
+            tags = {"channel", "post"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", content = { @Content(schema = @Schema(implementation = ChannelVideo.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", content = { @Content(schema = @Schema())})
+    })
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/{id}")
     public ChannelVideo sendChannel(@PathVariable String id,  @RequestParam(required = false,defaultValue = "10") int maxVideos,
                                     @RequestParam(required = false,defaultValue = "10") int maxComments){
